a85229cc266aee416c7e6bd0ccd21ba08ec34cb9
thuotracy <tracyjacobs775@gmail.com>
2024-07-09 16:22:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function cubes(){
+    let container = document.querySelector('.container');
+    let zValues = [-3,-2,-1,0,1,2,3];
+    zValues.forEach(z => {
+        let cube = document.
+    })
+}
