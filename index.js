13a887274fdd873855f769f620b9fadbfcf8965e
thuotracy <tracyjacobs775@gmail.com>
2024-07-09 16:24:47 +0300

    adding function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function cubes(){
     let container = document.querySelector('.container');
     let zValues = [-3,-2,-1,0,1,2,3];
     zValues.forEach(z => {
-        let cube = document.
+        let cube = document.createElement('div');
+        cube.classList.add('cube');
+        cube.style.setProperty('--z',z);
     })
 }
